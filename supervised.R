904f1a12570818971216129bfaa8c753f6b0d6fd
Bidias MELATAGUIA <bidiasmelataguia@MacBook-Pro-de-Bidias.local>
2024-12-19 23:29:43 +0100

    modify unsupervised and supervised

--- supervised.R
+++ supervised.R
@@ -1,3 +1,72 @@
-#charger notre dataset et l'observer 
-load("supervisee.RData")
-View(data)
+library(tidyverse)
+library(mlbench)  
+library(rpart)
+library(rpart.plot)
+library(randomForest)
+library(randomForestExplainer)
+library(ggplot2)
+
+# 1. Charger et observer les données ----
+load("supervisee.RData")  # Charger les données
+View(data)  # Observer les données
+summary(data)  # Voir le résumé des données
+
+# Vérifier la structure des données
+str(data)
+
+
+
+set.seed(123)
+
+# Diviser les données en ensemble d'entraînement et de test
+indTrain = sample(1:(dim(data)[1]), 850)
+dataTrain = data[indTrain, ]
+dataTest = data[-indTrain, ]
+
+# 2. Entraînement d'un modèle d'arbre de décision ----
+modele1 = rpart(target ~ ., data = dataTrain)
+
+# Visualisation de l'arbre de décision
+rpart.plot(modele1)
+
+# Prédictions avec le modèle
+predictions1proba = predict(modele1, newdata = dataTest)
+predictions1 = ifelse(predictions1proba[, 1] < 0.5, "R", "M") 
+
+# Afficher la matrice de confusion
+table(dataTest$Class, predictions1)
+
+# 3. Entraînement d'un modèle d'arbre de décision avec élargissement des paramètres ----
+modele2 = rpart(target ~ ., data = dataTrain, minsplit = 1, minbucket = 1, cp = 0.01)
+rpart.plot(modele2)
+
+predictions2proba = predict(modele2, newdata = dataTest)
+predictions2 = ifelse(predictions2proba[, 1] < 0.5, "R", "M")
+table(dataTest$target, predictions2)
+
+# 4. Entraînement d'un modèle Random Forest ----
+modele10 = randomForest(target ~ ., data = dataTrain)
+plot(modele10)  # Visualisation de l'importance des variables
+predictions10 = predict(modele10, newdata = dataTest)
+table(dataTest$target, predictions10)
+
+# 5. Entraînement d'un Random Forest avec mtry ajusté ----
+modele11 = randomForest(target ~ ., data = dataTrain, mtry = 30)
+plot(modele11)
+predictions11 = predict(modele11, newdata = dataTest)
+table(dataTest$target, predictions11)
+
+# 6. Interprétation du modèle Random Forest ----
+modele20 = randomForest(target ~ ., data = dataTrain, localImp = TRUE)
+
+# Visualisation de la distribution des profondeurs de l'arbre
+plot_min_depth_distribution(modele20)
+
+# Visualisation de l'importance des variables
+plot_multi_way_importance(modele20)
+
+# Visualisation des interactions entre variables
+plot_predict_interaction(modele20, data = dataTrain, variable1 = "V12", variable2 = "V11")
+
+# Plot d'un partial plot pour la variable "V1"
+partialPlot(modele20, x.var = "V1", pred.data = dataTrain)
--- unsupervised.R
+++ unsupervised.R
@@ -3,6 +3,9 @@ library(factoextra)
 library(cluster)
 library(ggplot2)
 library(datasets)
+library(dplyr)
+
+
 
 
 #charger notre dataset et l'observer 
@@ -40,6 +43,90 @@ fviz_dist(distance_matrix_normalized, gradient = list(low = "#00AFBB", mid = "wh
 
 
 
+# 3. Nombre optimal de clusters : méthode du coude ----
+# Methode du coude à la main
+valeursK = 2:15
+sommeCarresTotale = array(NA, length(valeursK))
+
+for (i in 1:length(valeursK)){
+  sommeCarresTotale[i] = kmeans(data_normalized, centers = valeursK[i], nstart = 100)$tot.withinss
+}
+
+toPlot = tibble(K = valeursK, metrique = sommeCarresTotale)
+ggplot(toPlot) + 
+  geom_point(aes(x = K, y = metrique)) + 
+  geom_line(aes(x = K, y = metrique))
+
+# Methode du coude avec une fonction
+fviz_nbclust(data_normalized, FUNcluster = kmeans, method = "wss", k.max = 15)
+
+
+
+
+
+
+
+
+
+
+
+# Créer un vecteur pour les valeurs de K à tester (par exemple de 1 à 16)
+K_values <- 1:16
+
+# Créer un vecteur pour stocker les valeurs de tot.withinss (somme des distances intra-clusters)
+dintra_values <- numeric(length(K_values))
+
+# Boucle pour appliquer K-means et extraire la valeur de tot.withinss
+for (i in 1:length(K_values)) {
+  kmeans_result <- kmeans(data_normalized, centers = K_values[i], nstart = 25)
+  dintra_values[i] <- kmeans_result$tot.withinss
+}
+
+# Visualiser les résultats (somme des distances intra-clusters en fonction de K)
+plot(K_values, dintra_values, type = "b", pch = 19, col = "blue", 
+     xlab = "Nombre de clusters (K)", ylab = "Totale des distances intra-cluster",
+     main = "Elbow Method (Coude) pour K-means", xaxt = "n")  # Empêcher la génération automatique de l'axe x
+
+# Ajouter un axe x avec des valeurs de 1 à 16
+axis(1, at = K_values, labels = K_values)
+
+
+
+
+
+
+
+
+
+# Créer un vecteur pour les valeurs de K à tester (par exemple de 2 à 15)
+K_values <- 2:15
+
+# Créer un vecteur pour stocker les scores moyens de silhouette
+silhouette_scores <- numeric(length(K_values))
+
+# Boucle pour appliquer K-means et calculer le score moyen de silhouette
+for (i in 1:length(K_values)) {
+  # Appliquer K-means avec un nombre de clusters spécifique
+  kmeans_result <- kmeans(data_normalized, centers = K_values[i], nstart = 25)
+  
+  # Calcul de la matrice de distance entre les points
+  distance_matrix <- get_dist(data_normalized, method = "euclidean")
+  
+  # Calcul des scores de silhouette
+  silhouette_result <- silhouette(kmeans_result$cluster, distance_matrix)
+  
+  # Calcul de la moyenne des scores de silhouette
+  silhouette_scores[i] <- mean(silhouette_result[, 3])
+}
+
+# Visualiser les résultats (score de silhouette moyen en fonction de K)
+plot(K_values, silhouette_scores, type = "b", pch = 19, col = "blue", 
+     xlab = "Nombre de clusters (K)", ylab = "Score moyen de la silhouette",
+     main = "Méthode de la silhouette pour déterminer le nombre optimal de clusters")
+
+
+
+
 # Appliquer K-means sur les données normalisées avec 2 clusters
 set.seed(123)  # Pour des résultats reproductibles
 kmeans_result_normalized <- kmeans(data_normalized, centers = 2, nstart = 25)
@@ -56,14 +143,52 @@ fviz_cluster(kmeans_result_normalized, data = data_normalized, geom = "point", m
 kmeans_result_3_normalized <- kmeans(data_normalized, centers = 3, nstart = 25)
 fviz_cluster(kmeans_result_3_normalized, data = data_normalized, geom = "point", main = "K-means avec 3 clusters sur données normalisées")
 
-# Appliquer K-means avec 4 clusters sur les données normalisées
-kmeans_result_4_normalized <- kmeans(data_normalized, centers = 4, nstart = 25)
+# Appliquer K-means avec 5 clusters sur les données normalisées
+kmeans_result_4_normalized <- kmeans(data_normalized, centers = 5, nstart = 100)
 fviz_cluster(kmeans_result_4_normalized, data = data_normalized, geom = "point", main = "K-means avec 4 clusters sur données normalisées")
 
 
 
-# Appliquer K-means avec 6 clusters et nstart = 25 sur les données normalisées
-kmeans_result_6_normalized <- kmeans(data_normalized, centers = 6, nstart = 25)
-fviz_cluster(kmeans_result_6_normalized, data = data_normalized, geom = "point", main = "K-means avec 6 clusters (stabilisé) sur données normalisées")
+
+
+
+
+
+
+# Appliquer K-means clustering (par exemple avec 4 clusters)
+kmeans_result <- kmeans(data_normalized, centers = 4, nstart = 25)
+
+# Visualisation des clusters
+fviz_cluster(kmeans_result, data = data_normalized,
+             geom = "point",  # Utiliser des points pour les observations
+             ellipse.type = "convex",  # Dessiner des ellipses convexes autour des clusters
+             main = "Visualisation des clusters avec K-means")
+
+# Ajouter les clusters aux données normalisées
+data_with_clusters <- data.frame(data_normalized, cluster = factor(kmeans_result$cluster))
+
+# Analyser chaque cluster de manière automatisée
+for (i in 1:4) {  # 4 correspond au nombre de clusters
+  cat("\nRésumé pour le Cluster", i, ":\n")
+  
+  # Filtrer les observations du cluster actuel
+  cluster_data <- data_with_clusters %>% filter(cluster == i)
+  
+  # Afficher un résumé statistique
+  print(summary(cluster_data))
+  
+  #  Enregistrer les données du cluster dans un fichier CSV
+  # write.csv(cluster_data, paste0("cluster_", i, ".csv"), row.names = FALSE)
+}
+
+
+
+
+
+
+
+
+
+
 
 
